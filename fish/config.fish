9a4b3ed74920c761b245ef1144946f49c6b26912
nizkcs <anhtungtom@proton.me>
2024-11-30 21:37:23 +0700

    fish

--- fish/config.fish
+++ fish/config.fish
@@ -24,8 +24,7 @@ abbr .5 'cd ../../../../..'
 abbr mkdir 'mkdir -p'
 
 # Alias 
-alias l='eza -lh  --icons=auto' # long list
 alias ls='eza -1   --icons=auto' # short list
-alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
-alias ld='eza -lhD --icons=auto' # long list dirs
-alias lt='eza --icons=auto --tree' # list folder as tree
+alias la='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias ld='eza -lhDa --icons=auto' # long list dirs
+alias lt='eza -a --icons=auto --tree' # list folder as tree
